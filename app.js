f290f912ea60d718df80c73b2e79324ccabc8de8
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-05-03 21:40:50 -0500

    Agregando endpoint POST

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 // App de express
 const app = express()
 app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
 
 // ESTO ES UN ENDPOINT
 // routes
@@ -17,8 +19,19 @@ app.get('/v1/explorers', (req, res) => {
     res.status(200).json(explorers)
 })
 
-// Puerto en que vamos a ver nuestra app: localhost:3000
-const port = 3000
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`GET BY ID Explorers V1 API \n${new Date()}`)
+    const explorer1 = { id: 1, name: "Explorer 1" }
+    //HTTP CODE STATUS: 200
+    res.status(200).json(explorer1)
+})
+
+app.post('/v1/explorers', (req, res) => {
+    console.log(`POST Explorers V1 API ${new Date()}`)
+    // Agregar la lógica para persistir
+    console.log(req.body) // parámetros del request
+    res.status(201).json({message: "Creado exitosamente"})
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
